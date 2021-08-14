c902976a404f115a80976ba637ad09976b2f0ff1
evgeniikozhanov <evgenii.kozhanov@gmail.com>
2021-08-14 22:17:26 +0300

    some doubts

--- tests/unit/test_c3t6_group_properties.py
+++ tests/unit/test_c3t6_group_properties.py
@@ -2,6 +2,9 @@ from c3t6_group.c3t6_group import C3T6Item
 
 
 def test_c3t6_group_constitutive_relation(c3t6_simple_group):
+    """
+    Does it guarantee that C(3) is satisfied?
+    """
 
     assert c3t6_simple_group.constitutive_relations == {
         (C3T6Item('a', False), C3T6Item('c', False), C3T6Item('b', False)),
